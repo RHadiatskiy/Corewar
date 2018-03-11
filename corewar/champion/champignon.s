a8e91e61840ae8e0f05b7c965e57b1896107247e
Roman Hadiatskyi <rhadiats@e1r9p3.unit.ua>
2018-03-11 21:33:32 +0200

    champions are added/just template

---
+++ corewar/champion/champignon.s
@@ -0,0 +1,7 @@
+# We are the Champignons
+
+.name "Champignon!"
+.comment "We are the Champignons!"
+
+live:	live %1
+		zjmp %:live
---
+++ corewar/champion/liver.s
@@ -0,0 +1,10 @@
+# Live for Live
+
+.name "Liver!"
+.comment "Live is .."
+
+live:	live %-1
+
+store:	st r1,5
+
+fork:	fork %:live		
